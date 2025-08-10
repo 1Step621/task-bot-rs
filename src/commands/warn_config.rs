36d9170d9ef51d3b7813e82609b3461e458ae355
1Step621 <onestep621@gmail.com>
2025-08-11 02:45:49 +0900

    enhance: command description

--- src/commands/warn_config.rs
+++ src/commands/warn_config.rs
@@ -4,6 +4,7 @@ use poise::serenity_prelude::*;
 use crate::{PoiseContext, data};
 
 #[poise::command(slash_command, dm_only)]
+/// 宿題の期限接近通知を有効にします。DMで実行してください。
 pub async fn enable_warn(ctx: PoiseContext<'_>) -> Result<(), Error> {
     ctx.data()
         .warn_users
@@ -25,6 +26,7 @@ pub async fn enable_warn(ctx: PoiseContext<'_>) -> Result<(), Error> {
 }
 
 #[poise::command(slash_command, dm_only)]
+/// 宿題の期限接近通知を無効にします。DMで実行してください。
 pub async fn disable_warn(ctx: PoiseContext<'_>) -> Result<(), Error> {
     ctx.data()
         .warn_users
